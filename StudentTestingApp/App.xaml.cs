5811ff08807db3f18ebadc2af9bd746e3104a4f6
DevOpsIzvrashuga2005 <finalichenie@gmail.com>
2025-06-08 21:23:16 +0300

    Clean up README and document connection string

--- StudentTestingApp/App.xaml.cs
+++ StudentTestingApp/App.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows;
 using Microsoft.EntityFrameworkCore;
 using StudentTestingApp.Models;
@@ -7,17 +8,30 @@ namespace StudentTestingApp
     public partial class App : Application
     {
         public StudentTestingContext Db { get; private set; } = null!;
+        public User? CurrentUser { get; set; }
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            var options = new DbContextOptionsBuilder<StudentTestingContext>()
-                .UseNpgsql("Host=localhost;Database=testing;Username=postgres;Password=secret")
-                .Options;
+            try
+            {
+                string connString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+                    ?? "Host=localhost;Database=testing;Username=postgres;Password=secret";
 
-            Db = new StudentTestingContext(options);
-            Db.Database.Migrate();
+                var options = new DbContextOptionsBuilder<StudentTestingContext>()
+                    .UseNpgsql(connString)
+                    .Options;
+
+                Db = new StudentTestingContext(options);
+                Db.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to initialize database: {ex.Message}", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                Shutdown();
+            }
         }
     }
 }
